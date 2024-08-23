afdf83028d3ef9c2db218c60e61043e1b81628a3
Anton <StarAntonU@gmail.com>
2024-08-23 16:54:32 +0300

    create Book, BookRepository, BookService and impl them

--- src/main/java/mate/academy/BookstoreApplication.java
+++ src/main/java/mate/academy/BookstoreApplication.java
@@ -1,11 +1,35 @@
 package mate.academy;
 
+import java.math.BigDecimal;
+import mate.academy.model.Book;
+import mate.academy.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
+    @Autowired
+    private BookService bookService;
+
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("Title");
+            book.setPrice(new BigDecimal("1000"));
+            book.setIsbn("abc");
+            book.setAuthor("Stepan Giga");
+
+            bookService.save(book);
+
+            bookService.findAll().forEach(System.out::println);
+        };
     }
 }
