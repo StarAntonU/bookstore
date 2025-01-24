a793999ab43f0fea7902d5339b524971e755c477
Anton <StarAntonU@gmail.com>
2025-01-25 00:52:32 +0200

    add handlerEntityNotFoundException

--- src/main/java/mate/academy/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -1,23 +1,24 @@
 package mate.academy.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
     @NotBlank
-    @NotNull
+    @Size(min = 3, max = 64)
     private String title;
     @NotBlank
-    @NotNull
+    @Size(min = 3, max = 64)
     private String author;
-    @NotNull
+    @NotBlank
+    @Size(min = 10, max = 64)
     private String isbn;
-    @Min(0)
-    @NotNull
+    @NotBlank
+    @Positive
     private BigDecimal price;
     private String description;
     private String coverImage;
