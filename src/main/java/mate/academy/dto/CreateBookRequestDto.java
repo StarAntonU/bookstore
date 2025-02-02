1a50ce49b60c43d8356fd560f401d4cf3fdca1c5
Anton <StarAntonU@gmail.com>
2025-02-02 23:18:45 +0200

    change @NotNull to @NotBlank in String

--- src/main/java/mate/academy/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -1,5 +1,6 @@
 package mate.academy.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
@@ -8,13 +9,13 @@ import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     @Size(min = 3, max = 64)
     private String title;
-    @NotNull
+    @NotBlank
     @Size(min = 3, max = 64)
     private String author;
-    @NotNull
+    @NotBlank
     @Size(min = 10, max = 64)
     private String isbn;
     @NotNull
