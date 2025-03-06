e06be86046564d4c65c15a445d8c1bf32511fd70
Anton <StarAntonU@gmail.com>
2025-03-06 21:30:48 +0200

    add method isEnabled() in User

--- src/main/java/project/bookstore/security/CustomUserDetailsService.java
+++ src/main/java/project/bookstore/security/CustomUserDetailsService.java
@@ -3,8 +3,8 @@ package project.bookstore.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import project.bookstore.exception.unchecked.EntityNotFoundException;
 import project.bookstore.repository.user.UserRepository;
 
 @Service
@@ -13,9 +13,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email)
-            throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) {
         return userRepository.findByEmail(email).orElseThrow(
-                () -> new UsernameNotFoundException("Can`t find user by email " + email));
+                () -> new EntityNotFoundException("Can`t find user by email " + email));
     }
 }
