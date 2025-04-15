3b9f89f34efcab59aa69b7a375c8bdf927f716c4
Anton <StarAtnonU@gmail.com>
2025-04-15 11:30:20 +0300

    uncommented test class

--- src/test/java/project/bookstore/BookstoreApplicationTests.java
+++ src/test/java/project/bookstore/BookstoreApplicationTests.java
@@ -1,11 +1,11 @@
 package project.bookstore;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookstoreApplicationTests {
-    //@Test
-    //void contextLoads() {
-    //}
+    @Test
+    void contextLoads() {
+    }
 }
